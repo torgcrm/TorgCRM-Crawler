4695757fa56ab1177dd6e47a6b85726445b59e2f
Ilya Durdyev <funbanji@gmail.com>
2018-02-25 13:15:42 +0300

    creation and updating timestamp on entities

--- src/main/java/ru/torgcrm/crawler/domain/BasicEntity.java
+++ src/main/java/ru/torgcrm/crawler/domain/BasicEntity.java
@@ -2,6 +2,8 @@ package ru.torgcrm.crawler.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.MappedSuperclass;
 import java.util.Date;
@@ -10,9 +12,11 @@ import java.util.Date;
 public abstract class BasicEntity {
     @Getter
     @Setter
+    @CreationTimestamp
     private Date createdDate;
     @Getter
     @Setter
+    @UpdateTimestamp
     private Date updatedDate;
 
     public abstract Long getId();
